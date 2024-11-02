94cb7a322dd68f31a08cd8791ce324ead7bd19c1
zogha <fz.bendehbi@gmail.com>
2024-11-02 13:43:47 +0100

    adding this

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,10 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +17,10 @@ function App() {
     <>
       <Header />
       <WelcomeMessage />
+      <UserProfile 
+      name="Zogha" 
+      age="2" 
+      bio="Loves coding and weight lifting" />
       <MainContent />
       <Footer />
 
