1f6d5fc775b03ed0d79c5bef4f00cb82646aed02
zogha <fz.bendehbi@gmail.com>
2024-11-09 14:22:00 +0100

    commit changes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{backgroundColor:'lightskyblue',padding:'10px',}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
