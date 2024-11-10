508280f6d24a5fa9a7ad31af0e39fef776ef0ed0
zogha <fz.bendehbi@gmail.com>
2024-11-10 16:06:45 +0100

    update

--- alx-react-app-new/alx-react-app-props/src/App.jsx
+++ alx-react-app-new/alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Alice", age: 25, bio: "Loves hiking and photography" };
