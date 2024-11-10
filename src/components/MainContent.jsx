9291eed9e014550791b39a73f1a8df14ef344b46
zogha <fz.bendehbi@gmail.com>
2024-11-10 15:58:06 +0100

    updateeeee

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor:'lightskyblue',padding:'10px',}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
