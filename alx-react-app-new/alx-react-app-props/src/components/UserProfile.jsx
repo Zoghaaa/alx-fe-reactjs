2985cc707c845b589dfcd2b5191b4792e7a59698
zogha <fz.bendehbi@gmail.com>
2024-11-10 19:26:16 +0100

    commit

--- alx-react-app-new/alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-new/alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
+function UserProfile() {
+    const {name, age, bio} = useContext(UserContext);
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Age: {age}</p>
+            <p>Bio: {bio}</p>
+        </div>
+    );
 };
 
 export default UserProfile;
