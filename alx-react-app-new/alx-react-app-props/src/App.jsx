771c914c51f5729163bd2a3132d77b48f526360c
zogha <fz.bendehbi@gmail.com>
2024-11-11 12:14:01 +0100

    commit

--- alx-react-app-new/alx-react-app-props/src/App.jsx
+++ alx-react-app-new/alx-react-app-props/src/App.jsx
@@ -1,25 +1,28 @@
-// src/App.jsx
+
 import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from './components/UserContext';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-  const userData = { name: "Alice", age: 25, bio: "Loves hiking and photography" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
-    <WelcomeMessage />
-    <Header />
-    <MainContent />
-    <Footer />
-    <UserContext.Provider value={userData}>
-      <UserProfile />
-    </UserContext.Provider>
-  </>
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+        <UserProfile />
+      </UserContext.Provider>
+
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
