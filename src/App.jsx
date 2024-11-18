d3eb9526a90a6d3fc419e4255f3f5f5275b8b8f3
zogha <fz.bendehbi@gmail.com>
2024-11-18 14:23:54 +0100

    add those files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
@@ -13,10 +12,10 @@ function App() {
     <BrowserRouter basename="/">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/services" element={<Services />}/>
-        <Route path="/contact" element={<Contact />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </BrowserRouter>
@@ -24,29 +23,3 @@ function App() {
 }
 
 export default App;
-=======
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
-
-function App() {
-  return (
-    <div>
-      <header style={{ textAlign: 'center', margin: '20px 0' }}>
-        <h1>Recipe Sharing Application</h1>
-      </header>
-
-      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        
-        <AddRecipeForm />
-
-        
-        <section style={{ marginTop: '20px', width: '80%' }}>
-          <RecipeList />
-        </section>
-      </main>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> aa9ec3e56c4e2d217635d2c2bf28056c3bfa892c
