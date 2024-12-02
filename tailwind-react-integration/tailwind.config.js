a81df47d81cec5d0cbc3ff415cf9b54e232df10f
zogha <fz.bendehbi@gmail.com>
2024-12-02 12:28:24 +0100

    commit

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Use 'content'
+  purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Older 'purge' key
+  darkMode: false, // Or 'class' if you use a CSS class to toggle dark mode
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {}, // Extend default variants if required
+  },
   plugins: [],
 };
+
