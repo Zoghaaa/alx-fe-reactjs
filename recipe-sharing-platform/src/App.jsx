7ddc76ac3731f820b2fc89193b244c6a87dc91a4
zogha <fz.bendehbi@gmail.com>
2024-12-02 13:38:56 +0100

    commit

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from './components/hello'; // Import the Hello component
+
+function App() {
+  return (
+    <>
+      <Hello /> {/* Use the Hello component */}
+    </>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-platform/src/components/hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  return (
+    <div className="text-blue-500 text-center">
+      <h1>Hello, Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+export default Hello;
