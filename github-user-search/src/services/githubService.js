567169cfd0b332860396c446853a3c422e945ddc
zogha <fz.bendehbi@gmail.com>
2024-12-15 23:11:00 +0100

    commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,21 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-const GITHUB_API_URL = 'https://api.github.com';
+export async function fetchUserData({ username, location, minRepos, page }) {
+    const queries = [];
 
-export const fetchUserData = async (username, location, minRepos) => {
-  try {
-    const query = [
-      username && `user:${username}`,
-      location && `location:${location}`,
-      minRepos && `repos:>=${minRepos}`,
-    ]
-      .filter(Boolean)
-      .join('+');
-    
-    const response = await axios.get(`${GITHUB_API_URL}/search/users?q=${query}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
+    if (username.trim().length) {
+        queries.push(username);
+    }
+
+    if (location.trim().length) {
+        queries.push(`location:${location}`);
+    }
+
+    if (minRepos.trim().length) {
+        queries.push(`repos:>${minRepos}`);
+    }
+
+    const queryString = queries.join(' ');
+
+    const url = `https://api.github.com/search/users?q=${encodeURIComponent(queryString)}&page=${page}&per_page=6`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
